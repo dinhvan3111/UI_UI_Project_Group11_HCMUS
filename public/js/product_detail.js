6ae5f4fdde7699f19259133e89cf7e274f1a5915
ngoctu280801 <63794942+ngoctu280801@users.noreply.github.com>
2022-07-15 11:49:53 +0700

    update notify in cart

--- public/js/product_detail.js
+++ public/js/product_detail.js
@@ -18,10 +18,32 @@ function addToCart(productId) {
         if(jsonResp.code === 200){
             $('#cartNum').val(jsonResp.totalInCart);
             $('#cartNumStr').html(jsonResp.totalInCart.toString());
-            alert(jsonResp.message);
+            //alert(jsonResp.message);
+            notify("success",jsonResp.message);
         }
         else{
-            alert(jsonResp.message);
+            notify("error",jsonResp.message);
         }
     });
 }
+
+function notify(type,message){
+    (()=>{
+      let n = document.createElement("div");
+      let id = Math.random().toString(36).substr(2,10);
+      n.setAttribute("id",id);
+      n.classList.add("notification",type);
+      n.innerText = message;
+      document.getElementById("notification-area").appendChild(n);
+      setTimeout(()=>{
+        var notifications = document.getElementById("notification-area").getElementsByClassName("notification");
+        for(let i=0;i<notifications.length;i++){
+          if(notifications[i].getAttribute("id") == id){
+            notifications[i].remove();
+            break;
+          }
+        }
+      },5000);
+    })();
+  }
+  
