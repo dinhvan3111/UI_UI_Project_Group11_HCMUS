d3a0e2fc8e1d7b34c952e078706694651d530d0d
vutuanhaigk123 <vutuanhaigk123@gmail.com>
2022-07-27 11:27:24 +0700

    fix bug init localStorage flow

--- middlewares/locals.mdw.js
+++ middlewares/locals.mdw.js
@@ -3,8 +3,8 @@ import categoryModel from '../models/category.model.js';
 import userModel from '../models/user.model.js';
 import cartModel from '../models/cart.model.js';
 
-export default function(app){
-    app.use(async function (req, res, next){
+export default function (app) {
+    app.use(async function (req, res, next) {
         const categories = await categoryModel.getAll();
         let categoriesRes = [];
         for (let i = 0; i < categories.length; i++) {
@@ -18,21 +18,30 @@ export default function(app){
         next();
     });
 
-    app.use(async function (req, res, next){
+    app.use(async function (req, res, next) {
         let result = 0;
-        if(typeof(req.session.passport) !== 'undefined'){
-            result = await cartModel.getTotalItems(req.session.passport.user._id);
+        if (typeof (req.session.passport) !== 'undefined') {
+            if (typeof (req.session.passport.user._id) !== 'undefined') {
+                result = await cartModel.getTotalItems(req.session.passport.user._id);
+            }
+            else {
+                result = await cartModel.getTotalItems(req.session.passport.user.data._id);
+            }
         }
         res.locals.lcTotalInCart = result;
         next();
     });
 
-    app.use(async function (req, res, next){
+    app.use(async function (req, res, next) {
         let authUser = req.session.passport;
-        if(typeof(authUser) === 'undefined'){
+        if (typeof (authUser) === 'undefined') {
             authUser = false;
         }
         else {
+            authUser.user = req.session.passport.user;
+            if (typeof (authUser.user.data) !== 'undefined') {
+                authUser.user = req.session.passport.user.data;
+            }
             const user = await userModel.findById(authUser.user._id);
             // req.user.id_permission = user.id_permission;
             authUser.user.id_permission = user.id_permission;
