429ea194de8190c07c3b913a963f2d16bfd4fff7
dinhvan3111 <85469453+dinhvan3111@users.noreply.github.com>
2022-07-30 15:47:50 +0700

    Add order-confirm page

--- routes/payment.route.js
+++ routes/payment.route.js
@@ -6,4 +6,8 @@ router.get('/check-out', function(req,res){
     res.render('vwPayment/check_out');
 });
 
+router.get('/order-confirm', function(req,res){
+    res.render('vwPayment/order_confirm');
+});
+
 export default router;
