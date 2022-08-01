9f749e5499c3742b2babc270b2671fdc397e8b42
dinhvan3111 <85469453+dinhvan3111@users.noreply.github.com>
2022-08-01 22:47:41 +0700

    POST list products in cart when click pay button

--- public/js/cart.js
+++ public/js/cart.js
@@ -6,23 +6,27 @@ async function sleep(seconds) {
 }
 
 payBtn.addEventListener('click', function (e) {
-    // const productId = document.querySelectorAll('.cart-detail .cart-info .cart-product-id');
-    // // const productName = document.querySelectorAll('.cart-detail .cart-info .cart-product-name');
-    // const productQuantity = document.querySelectorAll('.cart-detail .cart-product-quantity input');
 
-    // const result = [];
-    // for (let i = 0; i < productId.length; i++) {
-    //     var temp = { productId: productId[i].innerHTML, productQuantity: productQuantity[i].value };
-    //     result.push(temp);
-    // }
-    // for (let i = 0; i < result.length; i++) {
-    //     console.log(result[i]);
-    // }
+    // Post về server chi tiết cart để check-out
+    const productId = document.querySelectorAll('.cart-detail .cart-info .cart-product-id');
+    // const productName = document.querySelectorAll('.cart-detail .cart-info .cart-product-name');
+    const productQuantity = document.querySelectorAll('.cart-detail .cart-product-quantity input');
 
-    // var xhr = new XMLHttpRequest();
-    // xhr.open('POST', '/cart');
-    // xhr.setRequestHeader('Content-Type', 'application/json');
-    // xhr.send(JSON.stringify(result));
+    const result = [];
+    for (let i = 0; i < productId.length; i++) {
+        var temp = { productId: productId[i].innerHTML, productQuantity: productQuantity[i].value };
+        result.push(temp);
+    }
+    for (let i = 0; i < result.length; i++) {
+        console.log(result[i]);
+    }
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/cart');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(result));
+
+    // Xử lí ẩn hiện container cart và check-out
     const checkoutContainer = document.querySelector('.checkout-container');
     const cartContainer = document.querySelector('.cart-section');
     const title = document.querySelector('#title');
@@ -31,6 +35,7 @@ payBtn.addEventListener('click', function (e) {
     cartContainer.style.display = 'none';
     checkoutContainer.style.display = 'flex';
     window.scrollTo(0, 0);
+
 });
 
 async function showLoader(timeout){
